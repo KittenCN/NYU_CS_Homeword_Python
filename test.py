9de40370be2eea078b1f098f0d6e62716343ce2d
KittenCN <owdely@vip.163.com>
2022-11-16 23:07:00 +0800

    add new file

---
+++ test.py
@@ -0,0 +1,9 @@
+class people():
+       def __init__(self, name, age, height):
+                 self.name = name
+                 self.age = age
+                 self.height = height
+
+people1 = people("张三", 18, 180)  #声明一个people类的对象
+people1.name = "李四"  #修改对象的属性
+           
