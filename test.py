c89a8aaa36abf1eb38a8e048362b334622785649
KittenCN <owdely@vip.163.com>
2023-03-24 09:17:18 +0800

    new exam

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-a = 1
-a = 2
-b = str(3)
-
-def test(str):
-    print(str)
-
+for x in range (1,100):
+  for y in range(1,100):
+    z = 100 - x -y
+    if(5*x + 3*y + z/3 == 100) and z%3 ==0:
+        print(x,y,z)
